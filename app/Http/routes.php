38d4b9cf73ec9b77bd1fe31c6cbff76a4843f2c2
syfberryhzy <1913549290@mail.com>
2017-04-07 10:31:02 +0000

    Finish user signup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,20 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
+
+resource('users','UsersController');
+// 以上代码等同于
+// 显示所有用户列表的页面
+// get('/users','UsersController@index')->name('users.index');
+// 显示用户个人信息的页面
+// get('/users/{id}','UsersController@show')->name('users.show');
+// 创建用户的页面
+// get('/users/create','UsersController@create')->name('users.create');
+// 创建用户
+// post('/users','UsersController@store')->name('users.store');
+// 编辑用户个人资料的页面
+// get('/users/{id}/edit','UsersController@edit')->name('users.edit');
+// 更新用户
+// patch('/users/{id}','UsersController@update')->name('users.update');
+// 删除用户
+// delete('/users/{id}','UsersController@destroy')->name('users.destroy');
